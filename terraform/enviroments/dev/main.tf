d3748c142878e4c2d5801d224379ccea3c3738c1
JonathanJapa <jonathanprot.cad@gmail.com>
2024-01-11 22:55:23 -0300

    deleting tf state

--- terraform/enviroments/dev/main.tf
+++ terraform/enviroments/dev/main.tf
@@ -1,14 +1,32 @@
-module "converter" {
-  source         = "../../infra/converter"
-  environment    = var.environment
-
+data "aws_caller_identity" "current" {}
+output "arn" {
+  value = data.aws_caller_identity.current.arn
+  
+}
+output "account_id" {
+  value = data.aws_caller_identity.current.account_id
 }
 
-module "notifications"{
-  source         = "../../infra/notifications"
-  environment    = var.environment
+
+module "converter"{
+  source = "../../infra/converter"
+  environment = "${var.environment}"
+  aws_Access_key = "${var.aws_access_key}"
+  aws_secret_key = "${var.aws_secret_key}"
+  arn = data.aws_caller_identity.current.arn
+  account_id= data.aws_caller_identity.current.account_id
+  facture_queue_url = "${var.facture_queue_url}"
 }
 
-module "buckets" {
-  source         = "../../infra/buckets"
+
+module "notifications" {
+  source = "../../infra/notifications"
+  environment = "${var.environment}"
 }
+
+module "write" {
+  source = "../../infra/write"
+  environment = "${var.environment}"
+}
+
+
